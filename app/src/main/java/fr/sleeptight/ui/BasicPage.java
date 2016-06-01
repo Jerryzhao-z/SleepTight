0c5d825afadb71962373542c40c945ae75ff05f3
ZHOU <zqf110gx@outlook.com>
2016-06-01 23:22:18 +0200

    update Slide Menu : icon+switch

--- app/src/main/java/fr/sleeptight/ui/BasicPage.java
+++ app/src/main/java/fr/sleeptight/ui/BasicPage.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -17,13 +19,14 @@ import fr.sleeptight.ui.page.Page1;
 import fr.sleeptight.ui.page.Page2;
 
 public class BasicPage extends AppCompatActivity {
-
+    private Drawer result = null;
 
     public void Slide_Bar(Toolbar toolbar){
         new DrawerBuilder().withActivity(this).build();
-        PrimaryDrawerItem item0 = new PrimaryDrawerItem().withName(R.string.drawer_item_0).withIdentifier(0);
-        PrimaryDrawerItem item1 = new SecondaryDrawerItem().withName(R.string.drawer_item_1).withIdentifier(1);;
-        PrimaryDrawerItem item2 = new SecondaryDrawerItem().withName(R.string.drawer_item_2).withIdentifier(2);;
+        PrimaryDrawerItem item0 = new PrimaryDrawerItem().withName(R.string.drawer_item_0).withIdentifier(0).withIcon(FontAwesome.Icon.faw_home);
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName(R.string.drawer_item_1).withIdentifier(1).withIcon(FontAwesome.Icon.faw_eye);
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withName(R.string.drawer_item_2).withIdentifier(2).withIcon(FontAwesome.Icon.faw_file_text_o);
+        PrimaryDrawerItem item3 = new PrimaryDrawerItem().withName(R.string.drawer_item_3).withIdentifier(3).withIcon(FontAwesome.Icon.faw_file_text_o);
 
         Drawer result = new DrawerBuilder()
                 .withActivity(this)
@@ -32,30 +35,48 @@ public class BasicPage extends AppCompatActivity {
                         item0,
                         new DividerDrawerItem(),
                         item1,
-                        item2
-                )
+                        item2,
+                        item3
+                        )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-
-                        switch((int) drawerItem.getIdentifier()){
-                            case 0:
-                                Intent newAct0  = new Intent(getApplicationContext(), HomePage.class);
-                                startActivity(newAct0);break;
-                            case 1:
-                                Intent newAct1  = new Intent(getApplicationContext(), Page1.class);
-                                startActivity(newAct1);break;
-                            case 2:
-                                Intent newAct2  = new Intent(getApplicationContext(), Page2.class);
-                                startActivity(newAct2);break;
-                            default: break;
-                        }
-                        return true;
+						
+						if (drawerItem != null){
+							Intent intent = null;
+							switch((int) drawerItem.getIdentifier()){
+								case 0:
+									intent  = new Intent(getApplicationContext(), HomePage.class);
+									break;
+								case 1:
+									intent  = new Intent(getApplicationContext(), Page1.class);
+									break;
+								case 2:
+									intent  = new Intent(getApplicationContext(), Page2.class);
+									break;
+								default: break;
+							}
+							if (intent != null) {
+                                BasicPage.this.startActivity(intent);
+                            }
+						}
+							
+                        return false;
                     }
                 })
+                .withShowDrawerOnFirstLaunch(true)
                 .build();
 
+    }
 
+	@Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
