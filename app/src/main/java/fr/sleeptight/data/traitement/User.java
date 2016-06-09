0cfa2cf4e385121585155c06e6363062f25be4a4
Jerryzhao-z <5126582@163.com>
2016-06-09 16:49:17 +0200

    login

--- app/src/main/java/fr/sleeptight/data/traitement/User.java
+++ app/src/main/java/fr/sleeptight/data/traitement/User.java
@@ -34,7 +34,7 @@ public class User {
 
     private static RequestQueue requestQueue = NoHttp.newRequestQueue();
     private static final int SIGNUP = 0x0001;
-    private OnResponseListener<JSONObject> authResponseListener;
+    private OnResponseListener<JSONObject> authResponseListener, loginResponseListener;
     private User(){}
 
     private User(String userName, String pw)
@@ -70,7 +70,7 @@ public class User {
         return this;
     }
 
-    public User setId()
+    public User setId() // = sign up in server
     {
         try {
             this.createAccountInServer();
@@ -94,17 +94,17 @@ public class User {
             JsonRequest request = new JsonRequest("http://sleeptight2016.herokuapp.com/api/v1.0/users/", RequestMethod.POST);
             request.setRequestBody(json_request.getBytes(NoHttp.CHARSET_UTF8));
             request.setContentType("application/json");
-            authResponseListener = new JsonResponseListener(this);
+            authResponseListener = new JsonSignUpResponseListener(this);
             requestQueue.add(SIGNUP, request, authResponseListener);
         }
         return this;
     }
 
-    class JsonResponseListener implements OnResponseListener<JSONObject>
+    class JsonSignUpResponseListener implements OnResponseListener<JSONObject>
     {
         private User user;
 
-        public JsonResponseListener(User user)
+        public JsonSignUpResponseListener(User user)
         {
             this.user = user;
         }
@@ -142,13 +142,79 @@ public class User {
         public void onFinish(int what) {
             Log.d("SIGNUP:", "User sign up finished");
         }
-    };
+    }
 
+
+    /*login methods
+     *return id of user in server
+     *
+     */
+    public User login() throws UnsupportedEncodingException {
+        if(this.password != null && this.userName != null)
+        {
+            //json
+            SignupFormat user = new SignupFormat(this.userName, this.password);
+            String json_request = JSON.toJSONString(user);
+            JsonRequest request = new JsonRequest("http://sleeptight2016.herokuapp.com/api/v1.0/users/login", RequestMethod.POST);
+            request.setRequestBody(json_request.getBytes(NoHttp.CHARSET_UTF8));
+            request.setContentType("application/json");
+            loginResponseListener = new JsonLoginResponseListener(this);
+            requestQueue.add(SIGNUP, request, authResponseListener);
+        }
+        return this;
+    }
+
+    class JsonLoginResponseListener implements OnResponseListener<JSONObject>
+    {
+        private User user;
+
+        public JsonLoginResponseListener(User user)
+        {
+            this.user = user;
+        }
+
+        @Override
+        public void onStart(int what)
+        {
+            Log.d("LOGIN:", "User login started");
+        }
+
+
+        @Override
+        public void onSucceed(int what, Response<JSONObject> response) {
+            if(what == SIGNUP) {
+                try {
+                    String usernameReponse = response.get().getString("username");
+                    String idReponse = response.get().getString("id");
+                    if (usernameReponse == this.user.userName)
+                        this.user.id = idReponse;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.d("LOGIN:", "User login succeeded");
+
+        }
+
+        @Override
+        public void onFailed(int what, String url, Object tag, Exception exception, int responseCode, long networkMillis) {
+            Log.d("LOGIN:", "User login Faied");
+            Log.d("LOGIN:Failed", exception.getMessage());
+        }
+
+        @Override
+        public void onFinish(int what) {
+            Log.d("LOGIN:", "User login finished");
+        }
+    }
 
     //TODO: verifier s'il existe son id dans la base de donnée
     public boolean exists()
     {
-        return true;
+        if(id != null)
+            return true;
+        else
+            return false;
     }
 
     public User setRecentEvent(EventList recentEvents)
