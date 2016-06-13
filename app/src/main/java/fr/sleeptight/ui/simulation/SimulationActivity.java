e908e1285cb60d1c640e6d8a97ba767227ff4d06
Solicite <yifanxiong812@gmail.com>
2016-06-13 11:47:03 +0200

    ++Activity for simulation 6.13 11:46

--- app/src/main/java/fr/sleeptight/ui/simulation/SimulationActivity.java
+++ app/src/main/java/fr/sleeptight/ui/simulation/SimulationActivity.java
@@ -1,4 +1,4 @@
-package fr.sleeptight.ui.page;
+package fr.sleeptight.ui.simulation;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,34 +10,33 @@ import android.view.View;
 import android.widget.Button;
 
 import fr.sleeptight.R;
-import fr.sleeptight.ui.simulation.SimulationActivity;
+import fr.sleeptight.ui.BasicPage;
+import fr.sleeptight.ui.HomePage;
 
-/**
- * Created by Qifan on 2016/6/2.
- * cette interface a defini le sleep plan activity view
- */
-
-public class SleepPlanActivity extends AppCompatActivity {
-
+public class SimulationActivity extends BasicPage {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sleep_plan);
+        setContentView(R.layout.activity_simulation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Slide_Bar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Button simulation = (Button) findViewById(R.id.simulation_button);
-        simulation.setOnClickListener(new View.OnClickListener() {
+        Button backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SimulationActivity.class);
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+
+
+
+
     }
 
 }
